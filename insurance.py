95e2a6b2210b6b24cf1c216104b5e29352494542
Pho <115372840+phodmc@users.noreply.github.com>
2025-10-28 15:33:30 +0000

    feat: add shell context via flask shell

--- insurance.py
+++ insurance.py
@@ -1,1 +1,31 @@
-from app import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db
+from app.models import (
+    User,
+    Participant,
+    Policy,
+    Vehicle,
+    ParticipantGender,
+    TransactionBranch,
+    TransactionType,
+    CoverageType,
+    PolicyStatus,
+)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(
+        app=app,
+        db=db,
+        User=User,
+        Participant=Participant,
+        Policy=Policy,
+        Vehicle=Vehicle,
+        ParticipantGender=ParticipantGender,
+        TransactionBranch=TransactionBranch,
+        TransactionType=TransactionType,
+        CoverageType=CoverageType,
+        PolicyStatus=PolicyStatus,
+    )
