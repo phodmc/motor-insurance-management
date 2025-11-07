7ffd6b366c56d3bf26a92d37a7ab2eacc8e6fbb3
Pho <115372840+phodmc@users.noreply.github.com>
2025-11-07 16:46:52 +0000

    add method to create/return avatar url from gravatar

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from hashlib import md5
 from typing import Optional
 from werkzeug.security import generate_password_hash, check_password_hash
 import enum
@@ -88,6 +89,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode("utf-8")).hexdigest()
+        return f"https://www.gravatar.com/avatar/{digest}?d=identicon&size={size}"
 
     def __repr__(self):
         return f"<User {self.username}>"
